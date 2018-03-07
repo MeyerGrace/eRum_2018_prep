21ea0a67104e710f97a0acb76d79d2862abbf2fd
Kasia <katarzyna.kulma@gmail.com>
2018-03-07 13:52:37 +0000

    added first quanteda draft

---
+++ Kasia_quanteda_playground.R
@@ -0,0 +1,68 @@
+library(readr)
+library(quanteda)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(ggplot2)
+
+tweet_csv <- read_csv("tweets.csv")
+str(tweet_csv)
+
+table(tweet_csv$original_author)
+
+tweet_data <- select(tweet_csv,  author = handle, text, retweet_count, favorite_count, source_url, timestamp = time) %>% 
+  mutate(date = as_date(str_sub(timestamp, 1, 10))#,
+         #time = hms(str_sub(timestamp, 12, 19))
+  ) %>% select(-timestamp)
+
+table(tweet_data$author)
+summary(tweet_data)
+
+
+summary(tweet_corpus, n =nrow(tweet_data))
+
+tweet_corpus <- corpus(tweet_data)
+tweet_summary <- summary(tweet_corpus, n =nrow(tweet_data))
+str(tweet_summary)
+
+
+# subsetting corpus
+summary(corpus_subset(tweet_corpus, date > as_date('2016-07-01')), n =nrow(tweet_data))
+
+
+# checking context of a chosen word 
+
+kwic(tweet_corpus, "terror")
+kwic(tweet_corpus, "immigrant*")
+
+
+# visualize number and length of tweets 
+
+tweet_summary_tbl = tweet_summary %>% 
+  group_by(author, date) %>% 
+  summarize(no_tweets = n_distinct(Text),
+            avg_words = mean(Tokens),
+            avg_sentences = mean(Sentences))
+
+tweet_summary_tbl %>% 
+ggplot(aes(x = date, y = no_tweets, fill = author, colour = author)) +
+  geom_line() +
+  geom_point() 
+  
+
+tweet_summary_tbl %>% 
+  ggplot(aes(x = date, y = avg_words, fill = author, colour = author)) +
+  geom_line() +
+  geom_point() 
+
+
+tweet_summary_tbl %>% 
+  ggplot(aes(x = date, y = avg_sentences, fill = author, colour = author)) +
+  geom_line() +
+  geom_point() 
+
+
+# DFM
+# text cleaning
+# getting a wordcloud by author 
+# data prep for modeeling (dplyr?)
