fc4f770bc6e53b951d110f09500be15fb65142d5
Kasia <katarzyna.kulma@gmail.com>
2018-03-09 12:37:29 +0000

    added dfm and wordcloud bit

--- Kasia_quanteda_playground.R
+++ Kasia_quanteda_playground.R
@@ -62,7 +62,50 @@ tweet_summary_tbl %>%
   geom_point() 
 
 
-# DFM
+
+# create DFM
+
+my_dfm <- dfm(tweet_corpus)
+my_dfm[1:10, 1:5]
+
+# top features 
+
+topfeatures(my_dfm, 20)
+
 # text cleaning
-# getting a wordcloud by author 
+# edit tweets - remove URLs
+edited_dfm <- dfm(tweet_corpus, remove_url = TRUE, remove_punct = TRUE, remove = stopwords("english"))
+topfeatures(edited_dfm, 20)
+
+
+# getting a wordcloud
+
+set.seed(100)
+
+textplot_wordcloud(edited_dfm, min.freq = 20, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
+
+### getting a wordcloud by author
+## grouping by author - see differences!!!!
+by_author_dfm <- dfm(tweet_corpus,
+                     groups = "author",
+                     remove = stopwords("english"), remove_punct = TRUE, remove_url = TRUE)
+
+by_author_dfm[1:2,1:10]
+
+
+# wordcloud by author 
+
+
+set.seed(100)
+?textplot_wordcloud
+textplot_wordcloud(by_author_dfm,
+                   comparison = TRUE,
+                   min.freq = 50,
+                   random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
 # data prep for modeeling (dplyr?)
